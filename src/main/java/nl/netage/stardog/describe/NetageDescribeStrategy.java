7c4c75426298407f098bbea3a55f5324b08335b6
Nicky van Oorschot <nicky_van_oorschot@hotmail.com>
2018-05-22 13:44:10 +0200

    updated metafile name to the proper name and updated the dependencies to 5.3.0

--- src/main/java/nl/netage/stardog/describe/NetageDescribeStrategy.java
+++ src/main/java/nl/netage/stardog/describe/NetageDescribeStrategy.java
@@ -6,17 +6,12 @@ import com.complexible.common.base.CloseableIterator;
 import com.complexible.common.base.Streams;
 import com.complexible.common.openrdf.query.ImmutableDataset;
 import com.complexible.common.rdf.model.Namespaces;
-import com.complexible.stardog.StardogException;
-import com.complexible.stardog.index.IndexReader;
 import com.complexible.stardog.plan.describe.DescribeStrategy;
 import com.complexible.stardog.query.QueryFactory;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
-
-import org.openrdf.model.Model;
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
-import org.openrdf.model.Value;
 import org.openrdf.query.Dataset;
 import org.openrdf.query.GraphQueryResult;
 
@@ -40,33 +35,34 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 		// It should be otherwise queries like DESCRIBE :A FROM :g don't make sense but one may want to
 		// describe resources matched in G1 based on information in G2.
 		Dataset aDataset = ImmutableDataset.builder()
-		                                   .namedGraphs(Iterables.concat(theDataset.getDefaultGraphs(), theDataset.getNamedGraphs()))
-		                                   .build();
+				.namedGraphs(Iterables.concat(theDataset.getDefaultGraphs(), theDataset.getNamedGraphs()))
+				.build();
 		final GraphQueryResult aResults = theFactory.graph("construct {graph ?g { ?subject ?p ?object } } where { " +
-		                                             "graph ?g { " +
-		                                             "{ ?s ?p ?o . bind (?s as ?subject) bind(?o as ?object) } " +
-		                                             "union " +
-		                                             "{ ?o ?p ?s . bind (?o as ?subject) bind(?s as ?object) } } }",
-		                                             Namespaces.STARDOG)
-		                                      .dataset(aDataset)
-		                                      .parameter("s", theValue)
-		                                      .execute();
-		
+				"graph ?g { " +
+				"{ ?s ?p ?o . bind (?s as ?subject) bind(?o as ?object) } " +
+				"union " +
+				"{ ?o ?p ?s . bind (?o as ?subject) bind(?s as ?object) } } }",
+				Namespaces.STARDOG)
+				.dataset(aDataset)
+				.parameter("s", theValue)
+				.execute();
+
 		final GraphQueryResult bResults = theFactory.graph("CONSTRUCT {graph ?g {?o ?p2 ?o2}} WHERE {" 
 				+ "graph ?g {?s ?p ?o . " 
 				+ "?o ?p2 ?o2"
 				+ "filter(STRSTARTS(str(?o),str(?s)}}",                                                                                
-                Namespaces.STARDOG)
-         .dataset(aDataset)
-         .parameter("s", theValue)
-         .execute();
-		
+				Namespaces.STARDOG)
+				.dataset(aDataset)
+				.parameter("s", theValue)
+				.execute();
+
 		CloseableIterator<Statement> aResultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
+
 			public void close() {
 				aResults.close();
 			}
 
-			@Override
+
 			protected Statement computeNext() {
 				if (aResults.hasNext()) {
 					return aResults.next();
@@ -75,13 +71,13 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 				return endOfData();
 			}
 		};
-		
+
 		CloseableIterator<Statement> bResultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
 			public void close() {
 				bResults.close();
 			}
 
-			@Override
+
 			protected Statement computeNext() {
 				if (bResults.hasNext()) {
 					return bResults.next();
@@ -90,10 +86,13 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 				return endOfData();
 			}
 		};
-		
+
 		return Streams.concat(Streams.stream(aResultsIter), Streams.stream(bResultsIter));
 	}
 
+	public String getName() {
+		return "Example";
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -111,10 +110,5 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 	@Override
 	public boolean equals(final Object obj) {
 		return obj instanceof NetageDescribeStrategy;
-	}
-
-	public Model describe(IndexReader arg0, long[] arg1, Value arg2) throws StardogException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
