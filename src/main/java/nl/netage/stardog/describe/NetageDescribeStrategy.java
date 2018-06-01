471fb916145256e428b29bc2af7a31166c991d24
Nicky van Oorschot <nicky_van_oorschot@hotmail.com>
2018-06-01 16:23:38 +0200

    related to issue #2355

--- src/main/java/nl/netage/stardog/describe/NetageDescribeStrategy.java
+++ src/main/java/nl/netage/stardog/describe/NetageDescribeStrategy.java
@@ -1,5 +1,6 @@
 package nl.netage.stardog.describe;
 
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import com.complexible.common.base.CloseableIterator;
@@ -21,58 +22,39 @@ import org.openrdf.query.GraphQueryResult;
  * @author  Netage
  */
 public final class NetageDescribeStrategy implements DescribeStrategy {
-
+	static String BASE_SUBJECT = "";
 
 	@SuppressWarnings("unchecked")
 	public Stream<Statement> describe(final QueryFactory theFactory, final Dataset theDataset, final Resource theValue) {
 		// This class can be simplified by extending SingleQueryDescribeStrategy but we're showing the full version
 		// in case you want to do more than just run a single query.
 		Preconditions.checkArgument(theValue != null, "The described value should not be null");
-
+		BASE_SUBJECT = theValue.stringValue();
 		// The SPARQL spec doesn't define the dataset for DESCRIBE queries.
 		// FROM [NAMED] clauses in DESCRIBE queries define it for the WHERE pattern but
 		// it doesn't say whether the same dataset should be used for describing matched resources.
 		// It should be otherwise queries like DESCRIBE :A FROM :g don't make sense but one may want to
 		// describe resources matched in G1 based on information in G2.
+		
+		return Streams.stream(retrieveResults(theFactory, theDataset, theValue.stringValue()));
+		//return Streams.concat(Streams.stream(aResultsIter), Streams.stream(bResultsIter), Streams.stream(cResultsIter));
+	}
+	
+	private CloseableIterator<Statement> retrieveResults(final QueryFactory theFactory, final Dataset theDataset, final String theValue){
+		final CloseableIterator<Statement> streams;
+		
 		Dataset aDataset = ImmutableDataset.builder()
 				.namedGraphs(Iterables.concat(theDataset.getDefaultGraphs(), theDataset.getNamedGraphs()))
 				.build();
 		
-		final GraphQueryResult aResults = theFactory.graph("CONSTRUCT {?s ?p ?o ."
-				+ "?o ?p3 ?o3 ."
-				+ "} WHERE { Graph ?g {"
-				+ "?s ?p ?o ."
-				+ "OPTIONAL{?o ?p3 ?o3 ."
-				+ "filter(isBlank(?o))}	"
-				+ "}}",                                                                                
+		final GraphQueryResult aResults = theFactory.graph("CONSTRUCT {?s ?p ?o} WHERE { Graph ?g {"
+				+ "?s ?p ?o}}",                                                                                
 				Namespaces.STARDOG)
 				.dataset(aDataset)
 				.parameter("s", theValue)
 				.execute();
 		
-		final GraphQueryResult hashResults = theFactory.graph("CONSTRUCT {?nO ?nP ?o3} WHERE { Graph ?g{"
-				+ "?s ?p ?nO ."
-				+ "?nO ?nP ?o3 ."
-				+ "filter(STRSTARTS(str(?nO),?subjectString))}}",                                                                                
-				Namespaces.STARDOG)
-				.dataset(aDataset)
-				.parameter("s", theValue)
-				.parameter("subjectString", theValue.stringValue()+"#")
-				.execute();
-		
-		final GraphQueryResult blankNodeResults = theFactory.graph("CONSTRUCT {?o3 ?p4 ?o4 } WHERE { Graph ?g {"
-				+ "?s ?p ?nO . "
-				+ "?nO ?nP ?o3 ."
-				+ "?o3 ?p4 ?o4 ."
-				+ "filter(STRSTARTS(str(?nO),?subjectString))"
-				+ "filter(isBlank(?o3))}}",                                                                                
-				Namespaces.STARDOG)
-				.dataset(aDataset)
-				.parameter("s", theValue)
-				.parameter("subjectString", theValue.stringValue()+"#")
-				.execute();
-		
-		CloseableIterator<Statement> aResultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
+		CloseableIterator<Statement> resultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
 			public void close() {
 				aResults.close();
 			}
@@ -80,6 +62,11 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 			@Override
 			protected Statement computeNext() {
 				if (aResults.hasNext()) {
+					System.out.println(aResults.next().getObject().stringValue() + " - " + BASE_SUBJECT+"#");
+					if(aResults.next().getObject().stringValue().startsWith(BASE_SUBJECT+"#")){
+						streams = Streams.concat(streams,retrieveResults(theFactory, theDataset, aResults.next().getObject().stringValue()));
+					}
+
 					return aResults.next();
 				}
 
@@ -87,38 +74,9 @@ public final class NetageDescribeStrategy implements DescribeStrategy {
 			}
 		};
 		
-		CloseableIterator<Statement> bResultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
-			public void close() {
-				hashResults.close();
-			}
-
-			@Override
-			protected Statement computeNext() {
-				if (hashResults.hasNext()) {
-					return hashResults.next();
-				}
-
-				return endOfData();
-			}
-		};
-		
-		CloseableIterator<Statement> cResultsIter = new CloseableIterator.AbstractCloseableIterator<Statement>() {
-			public void close() {
-				blankNodeResults.close();
-			}
-
-			@Override
-			protected Statement computeNext() {
-				if (blankNodeResults.hasNext()) {
-					return blankNodeResults.next();
-				}
-
-				return endOfData();
-			}
-		};
-
-		return Streams.concat(Streams.stream(aResultsIter), Streams.stream(bResultsIter), Streams.stream(cResultsIter));
+		return Streams.stream(resultsIter);
 	}
+
 
 	public String getName() {
 		return "NetageDescribeStrategy";
